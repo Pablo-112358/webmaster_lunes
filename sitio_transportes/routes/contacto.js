fb526a163b966a39c0cd697c96f1da407d38661c
@Pablo-112358! <pabloalejandrorom@gmail.com>
2022-11-12 20:25:42 -0300

    Trabajo MOdulo 4 unidad 3

---
+++ sitio_transportes/routes/contacto.js
@@ -0,0 +1,48 @@
+var express = require("express");
+var router = express.Router();
+
+var nodemailer = require("nodemailer");
+
+/* GET home page. */
+
+router.get("/", function (req, res, next) {
+  res.render("contacto", { isContacto: true });
+});
+
+router.post("/", async (req, res, next) => {
+  //console.log(req.body)
+
+  var nombre = req.body.nombre;
+  var email = req.body.email;
+  var tel = req.body.tel;
+  var mensaje = req.body.mensaje;
+
+  var obj = {
+    to: "pabloromano087@gmail.com",
+    subject: "contacto desde la web",
+    html:
+      nombre +
+      " se ha contactado a travez de Transportes X y quere mas información a este correo: " +
+      email +
+      ".<br> Además, hizo este comentaario: " +
+      mensaje +
+      ",<br> su tel es: " +
+      tel,
+  };
+
+  var Transport = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS,
+    },
+  });
+  var info = await Transport.sendMail(obj);
+
+  res.render("contacto", {
+    isContacto:true,
+    message: "Mensaje enviado correctamente" });
+});
+
+module.exports = router;
